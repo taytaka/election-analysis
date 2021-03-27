706ac328c97c0d087a847a868076eea020f2d3aa
Taylor Takanishi <taylortakanishi@gmail.com>
2021-03-27 16:05:09 -0700

    Adding the election audit code

--- Python_practice.py
+++ Python_practice.py
@@ -17,3 +17,9 @@ for county_dict in voting_data:
   for value in county_dict.values():
     print(value)
 
+# Using the with statement, open the file as a text file
+# with open(file_to_save, "w") as txt_file:
+
+    # Write three counties to the file
+    # txt_file.write("Counties in the Election\n-------------------------\nArapahoe\nDenver\nJefferson")
+
